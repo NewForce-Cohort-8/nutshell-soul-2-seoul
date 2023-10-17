26db269592d778caf3d351b174b25f1c77031794
MaxAguilar712 <a26001977@gmail.com>
2023-10-17 10:25:36 -0400

    new chat v1

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,12 +1,14 @@
 // variable to store state after fetching from API
 const applicationState = {
     "news": [],
-    "chat": [],
     "events": [],
-    "tasks": []
+    "tasks": [],
+    "chats": []
 }
 
 const API = "http://localhost:8080"
+
+const mainContainer = document.querySelector(".dashboard")
 
 // fetch request data from API
 export const fetchNews = () => {
@@ -47,6 +49,57 @@ export const sendNews = (userNewsPost) => {
 // deletes news from API/json database which also removes the request from the browser 
 export const deleteNews = (id) => {
     return fetch(`${API}/news/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
+}
+
+
+
+
+//chats 
+
+// fetch request data from API
+export const fetchChats = () => {
+    return fetch(`${API}/chats`)
+        .then(response => {return response.json()})
+        .then(
+            (userChatsPost) => {
+                // Store the external state in application state
+                applicationState.chats = userChatsPost
+            }
+        )
+}
+
+// export chats in application state to make data renderable to HTML
+export const getChats = () => {
+    return applicationState.chats.map(chatspost => ({...chatspost}))
+}
+
+
+// sends chats post made by user in browser to API and then refactored to json database
+export const sendChats = (userChatsPost) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userChatsPost)
+    }
+
+
+    return fetch(`${API}/chats`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+// deletes chats from API/json database which also removes the request from the browser 
+export const deleteChats = (id) => {
+    return fetch(`${API}/chats/${id}`, { method: "DELETE" })
         .then(
             () => {
                 mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
